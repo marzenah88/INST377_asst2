67c9bf9f8d4a8def5faf2ba023ff57917c905460
Marzenah <marzenah123@gmail.com>
2020-11-07 09:39:09 -0500

    ll

--- public/asst-2/script.js
+++ public/asst-2/script.js
@@ -1,12 +1,12 @@
 
-  function narrowDataScope (data) {
-      const objectList = data.map(restaurant => ${data.name} ${data.address_line1} ${data.city} ${data.state} ${data.zip});
-      console.log(objectList)
-      return objectList;
-  } 
+function narrowDataScope (data) {
+    const objectList = data.map(restaurant => ${data.name} ${data.address_line1} ${data.city} ${data.state} ${data.zip});
+    console.log(objectList)
+    return objectList;
+} 
   
   
-  function getResults(jsonFromServer) {
+function getResults(jsonFromServer) {
     console.log('jsonFromServer', jsonFromServer);
     
     const narrowData = narrowDataScope(jsonFromServer);
@@ -14,17 +14,17 @@
     const options = makeYourOptionsObject(reorganizedData);
     const chart = new CanvasJS.Chart('chartContainer', options);
     chart.render();
-  }
-  function matchText(input_text, restaurants) {
-      return restaurants.filter(venue => {
-          const regex = new RegExp(wordToMatch, 'gi');
-          return venue.
-      })
-  }
+}
+function matchText(input_text, restaurants) {
+    return restaurants.filter(venue => {
+        const regex = new RegExp(wordToMatch, 'gi');
+        return venue.
+    })
+}
   
-  /// Leave lines 52-67 alone; do your work in the functions above
-  document.body.addEventListener('submit', async (e) => {
-    e.preventDefault(); // this stops whatever the browser wanted to do itself.
+  
+document.body.addEventListener('submit', async (e) => {
+    e.preventDefault(); 
     const form = $(e.target).serializeArray();
     const restaurants = [];
     fetch('/api', {
@@ -36,7 +36,7 @@
     })
       .then((fromServer) => fromServer.json())
       .then((jsonFromServer) => getResults(jsonFromServer))   
-        console.log(err);
-      });
+    console.log(err);
+});
 
-  );}
+  
