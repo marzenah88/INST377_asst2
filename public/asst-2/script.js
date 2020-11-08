8a9f0c8338cae03990b37250233f05ab1de00122
Chris Summers <Summersc98@users.noreply.github.com>
2020-11-07 22:20:53 -0500

    correctly commented out lines 46 and beyond

--- public/asst-2/script.js
+++ public/asst-2/script.js
@@ -43,9 +43,9 @@ function displayMatches() {
 
 
 
-/*
-/* This is just me trying to follow along with wesbos, sorry if I forgot to take it out before pushing -chris  */
-const wesbos_data = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+/**
+/* This is just me trying to follow along with wesbos, sorry if I forgot to take it out before pushing -chris
+/* const wesbos_data = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
 const lab_data = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
 
 const cities = [];
@@ -82,7 +82,7 @@ function displayMatches() {
     suggestions.innerHTML = html;
 }
 
-/* wesbos' input had a class of 'search' but ours is textinput */
+/* wesbos' input had a class of 'search' but ours is textinput
 const searchInput = document.querySelector('.textinput');
 const suggestions = document.querySelector('.suggestions');
 searchInput.addEventListener('input', displayMatches);
@@ -136,4 +136,3 @@ function getResults(jsonFromServer) {
 });
 
   */
-*/
