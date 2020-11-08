22ab8ee568d94fe8007d84dec23b01a180414911
Chris Summers <Summersc98@users.noreply.github.com>
2020-11-07 20:19:42 -0500

    Added WesBos Tutorial
    I mostly added this so that I could see what I was doing styling the css, but Y'all are welcome to use it or comment it away if you need to

--- public/asst-2/script.js
+++ public/asst-2/script.js
@@ -1,3 +1,53 @@
+/* This is just me trying to follow along with wesbos, sorry if I forgot to take it out before pushing -chris  */
+const wesbos_data = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+const lab_data = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
+
+const cities = [];
+
+fetch(wesbos_data)
+    .then(blob => blob.json())
+    .then(data => cities.push(...data))
+    ;
+
+function findMatches(wordsToMatch, cities) {
+    return cities.filter(place => {
+        const regex = new RegExp(wordsToMatch, 'gi');
+        if (wordsToMatch === "") {
+            return null
+        } else {
+            return place.city.match(regex) || place.state.match(regex);
+        }
+    })
+}
+
+function displayMatches() {
+    const matchArray = findMatches(this.value, cities);
+    const html = matchArray.map(place => {
+        const regex = new RegExp(this.value, 'gi');
+        const cityName = place.city.replace(regex, `<span class="highlightme">${this.value}</span>`);
+        const stateName = place.state.replace(regex, `<span class="highlightme">${this.value}</span>`);
+        return `
+            <li>
+                <span class="name">${cityName}, ${stateName}</span>
+                <span class="population">${place.population}</span>
+            </li>
+        `;
+    }).join('');
+    suggestions.innerHTML = html;
+}
+
+/* wesbos' input had a class of 'search' but ours is textinput */
+const searchInput = document.querySelector('.textinput');
+const suggestions = document.querySelector('.suggestions');
+searchInput.addEventListener('input', displayMatches);
+
+
+
+
+
+
+/*
+const { default: fetch } = require("node-fetch");
 
 function narrowDataScope (data) {
     const objectList = data.map(restaurant => ${data.name} ${data.address_line1} ${data.city} ${data.state} ${data.zip});
@@ -21,7 +71,7 @@ function getResults(jsonFromServer) {
         return venue.
     })
     */
-}
+
 
   
 /*document.body.addEventListener('submit', async (e) => {
