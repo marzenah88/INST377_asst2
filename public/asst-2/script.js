0c29fd2b9998243b8790a20b1e323926f3cd06ea
Marzenah <marzenah123@gmail.com>
2020-11-12 02:26:15 -0500

    pp

--- public/asst-2/script.js
+++ public/asst-2/script.js
@@ -1,49 +1,34 @@
-
-const lab_data = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
+const pg_data = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
 
 const venues = [];
 
-fetch(lab_data)
-    .then(blob => blob.json())
-    .then(data => venues.push(...data));
+fetch(pg_data)
+  .then(blob => blob.json())
+  .then(data => venues.push(...data))
 
 function findMatches(wordsToMatch, venues) {
-    return venues.filter(venue => {
-        const regex = new RegExp(wordsToMatch, 'gi');
-        if (wordsToMatch === "") {
-            return null;
-        }
-        return venue.name.match(regex) || venue.city.match(regex);
-    });
+  return venues.filter(venue => {
+    const regex = new RegExp(wordsToMatch, 'gi');
+    return venue.name.match(regex) || venue.city.match(regex)
+  });
 }
-
 function displayMatches() {
-    const matchArray = findMatches(this.value, venues);
-    const html = matchArray.map(venue => {
-        const regex = new RegExp(this.value, 'gi');
-        const venueName = venue.name.replace(regex, `<span class="highlightme">${this.value}</span>`);
-        const venueCity = venue.city.replace(regex, `<span class="highlightme">${this.value}</span>`);
-        return `
-            <li>
-                <span class="population">${venueName}</span>
-                <span class="name">${venueCity}, ${venue.zip}</span>
-                <span class="population">${venue.category}</span>
-                <span class="population">${venue.inspection_results}</span>
-            </li>
-        `;
-    }).join('');
-    suggestions.innerHTML = html;
+  const matchArray = findMatches(this.value, venues);
+  const html = matchArray.map(venue => {
+    const regex = new RegExp(this.value, 'gi');
+    const venueName = venue.name.replace(regex, `<span class="highlightme">${this.value}</span>`);
+    const venueCity = venue.city.replace(regex, `<span class="highlightme">${this.value}</span>`);
+    return `
+      <li>
+        <span class="name">${venueName.toLowerCase()},  </span>
+        <span class="cityAndZip">${venueCity.toLowerCase()},  ${'MD '}${venue.zip},  </span>
+        <span class="establishmentType">${venue.category},  </span>
+        <span class="inspectionResults">${venue.inspection_results}</span>
+      </li>
+    `;
+  }).join('');
+  suggestions.innerHTML = html;
 }
-
-/*wesbos' input had a class of 'search' but ours is textinput */
 const searchInput = document.querySelector('.textinput');
 const suggestions = document.querySelector('.suggestions');
 searchInput.addEventListener('input', displayMatches);
-
-
-
-
-
-
-
-
